69629b23e70b0c85e154f5c92cda0cb25e082a43
Jolyn Tan <jolyn.tan.sq@gmail.com>
2020-10-18 16:37:49 +0800

    Add bid modal with date range picker

--- client/src/components/careTakerAdmin/PetCareList.jsx
+++ client/src/components/careTakerAdmin/PetCareList.jsx
@@ -5,13 +5,26 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
+import Modal from '@material-ui/core/Modal';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import AddIcon from '@material-ui/icons/Add';
 import PetsIcon from '@material-ui/icons/Pets';
 import DeleteIcon from '@material-ui/icons/Delete';
+import BidModal from '../userProfile/careTakerProfile/BidModal';
 
 const PetCareList = (props) => {
+    const { owner, ...other} = props;
+    const [open, setOpen] = React.useState(false);
+
+    const openModal = () => {
+        setOpen(true);
+    }
+    
+    const closeModal = () => {
+        setOpen(false);
+    }
+
     return (
         <div>
             <List>
@@ -47,12 +60,17 @@ const PetCareList = (props) => {
                 </ListItem> : null }
                 {!props.owner ? 
                 <ListItemSecondaryAction>
-                    <IconButton>
+                    <IconButton onClick={openModal}>
                         <ListItemText  
                             primary="Bid"/>
                     </IconButton>
                 </ListItemSecondaryAction> : null } 
             </List>
+            <Modal
+                open={open}
+                onClose={closeModal}>
+                <BidModal modalHandler={closeModal}/>
+            </Modal>
         </div>
     )
 }
