b866ff27aead609a6dbbaca855fba9afa43c47a3
Sam-limyr <nacuniel@gmail.com>
2020-10-31 03:24:02 +0800

    Cause all procedures and functions to drop at the start of init.sql.

--- server/database/init.sql
+++ server/database/init.sql
@@ -10,11 +10,45 @@ DROP TABLE IF EXISTS Owned_Pet_Belongs CASCADE;
 DROP TABLE IF EXISTS Bid CASCADE;
 DROP VIEW IF EXISTS Users CASCADE;
 DROP VIEW IF EXISTS Accounts CASCADE;
-DROP PROCEDURE IF EXISTS add_bid(character varying,character varying,character varying,character varying,date,date);
-DROP PROCEDURE IF EXISTS add_bid;
-DROP PROCEDURE IF EXISTS add_fulltimer;
-DROP PROCEDURE IF EXISTS add_parttimer;
-DROP PROCEDURE IF EXISTS add_petOwner;
+--DROP PROCEDURE IF EXISTS add_bid(character varying,character varying,character varying,character varying,date,date) CASCADE;
+--DROP PROCEDURE IF EXISTS add_bid(unknown, unknown, unknown, unknown, unknown, unknown) CASCADE;
+--DROP PROCEDURE IF EXISTS add_bid CASCADE;
+--DROP PROCEDURE IF EXISTS add_fulltimer CASCADE;
+--DROP PROCEDURE IF EXISTS add_parttimer CASCADE;
+--DROP PROCEDURE IF EXISTS add_petOwner CASCADE;
+
+/* The code block below drops all functions, aggregates, and procedures from the database.
+   This is required because PostgreSQL can't handle overloaded functions and procedures. */
+DO
+$do$
+DECLARE
+   _sql text;
+BEGIN
+   SELECT INTO _sql
+          string_agg(format('DROP %s %s;'
+                          , CASE prokind
+                              WHEN 'f' THEN 'FUNCTION'
+                              WHEN 'a' THEN 'AGGREGATE'
+                              WHEN 'p' THEN 'PROCEDURE'
+                              WHEN 'w' THEN 'FUNCTION'  -- window function (rarely applicable)
+                              -- ELSE NULL              -- not possible in pg 11
+                            END
+                          , oid::regprocedure)
+                   , E'\n')
+   FROM   pg_proc
+   WHERE  pronamespace = 'public'::regnamespace  -- schema name here!
+   -- AND    prokind = ANY ('{f,a,p,w}')         -- optionally filter kinds
+   ;
+
+   IF _sql IS NOT NULL THEN
+       RAISE NOTICE '%', _sql;  -- debug / check first
+       EXECUTE _sql;         -- uncomment payload once you are sure
+   ELSE
+       RAISE NOTICE 'No fuctions found in schema %', quote_ident(_schema);
+   END IF;
+END
+$do$;
+
 
 CREATE TABLE PCSAdmin (
     username VARCHAR(50) PRIMARY KEY,
