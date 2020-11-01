b4d3d3eafca74a3dfd613c3c13d8ed9c19cb8c91
Fyonn <fyonn@Fyonns-MacBook-Pro.local>
2020-11-02 02:03:10 +0800

    dummy commit

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (name) VALUES ('Hung');
-INSERT INTO users (name) VALUES ('Jolyn');
-INSERT INTO users (name) VALUES ('Sam');
-INSERT INTO users (name) VALUES ('Fyonce');
-INSERT INTO users (name) VALUES ('Cheryl');
+-- INSERT INTO users (name) VALUES ('Hung');
+-- INSERT INTO users (name) VALUES ('Jolyn');
+-- INSERT INTO users (name) VALUES ('Sam');
+-- INSERT INTO users (name) VALUES ('Fyonce');
+-- INSERT INTO users (name) VALUES ('Cheryl');
