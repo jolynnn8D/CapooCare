36198e20f9675c772226d47df9119cef982e73a4
Fyonn <fyonn@Fyonns-MacBook-Pro.local>
2020-11-14 18:37:17 +0800

    new build

---
+++ client/build/precache-manifest.55a7f7c1c63b1abda120d8a5220ba3d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbc83c3b9df9601b6d76ea05bf50919",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b11b37d056163974ff29",
+    "url": "/static/css/2.d29b29af.chunk.css"
+  },
+  {
+    "revision": "b11b37d056163974ff29",
+    "url": "/static/js/2.14b4baff.chunk.js"
+  },
+  {
+    "revision": "d86f5b8eb337307abea661d60b0d995c",
+    "url": "/static/js/2.14b4baff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8688ae2244a6a2d3538",
+    "url": "/static/js/main.9d98c2fb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "fb90d3e65430f1dfd1be047d1321e59c",
+    "url": "/static/media/pet.fb90d3e6.png"
+  },
+  {
+    "revision": "f9f267908e3859b5815031d867677ccc",
+    "url": "/static/media/userProfile.f9f26790.png"
+  }
+]);
