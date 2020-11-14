7f6ca1b366f4ab412fa1e20fe85388136dbd8e9c
Fyonn <fyonn@Fyonns-MacBook-Pro.local>
2020-11-14 16:48:09 +0800

    configure init.js

--- database/init.js
+++ database/init.js
@@ -1,15 +1,26 @@
 // connect express to postgres
+require('dotenv').config();
 const { Pool } = require('pg');
 const fs = require('fs');
 const keys = require('../keys')
 
-const pool = new Pool({
+const devConfig = {
     user: keys.pgUser,
     host: keys.pgHost,
     database: keys.pgDatabase,
     password: keys.pgPassword,
-    port: keys.pgPort,
-});
+    port: keys.pgPort
+};
+
+const proConfig = {
+    connectionString: process.env.DATABASE_URL //heroku addons
+};
+
+console.log(process.env.NODE_ENV);
+
+const pool = new Pool(
+    process.env.NODE_ENV === "production" ? proConfig : devConfig
+);
 
 
 // Initialize database
